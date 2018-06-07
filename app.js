a05c9d74496e081c04757f7b26e9c4265fadefec
Talon <pvpgamecrash@gmail.com>
2018-06-06 22:25:56 -0700

    another serverside fix

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
