7d5fc9f2efa56499b75db2f555e8614ae7d1c256
Talon <pvpgamecrash@gmail.com>
2018-06-06 22:54:27 -0700

    css

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.get('/', function(req, res) {
     res.sendFile('views/index.html', { root: __dirname });
 });
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
