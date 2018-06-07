1ee37f917cd04d8a05ce9d11743dbebd1fca3b0c
Talon <pvpgamecrash@gmail.com>
2018-06-06 22:20:10 -0700

    little bit of a server side fix

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const path = require('path');
 
-app.get('/', function(req, res){
-    res.sendfile('index.html', { root: __dirname + "/views" } );
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 });
+
+app.listen(8080);
