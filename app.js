5ec00b8af7e9f9b350b095c5f89b683cf7262178
Talon <pvpgamecrash@gmail.com>
2018-06-06 22:30:52 -0700

    serverside fix

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+var app = express();
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + './views/index.html'));
 });
